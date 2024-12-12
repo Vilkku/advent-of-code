0a66a2e2cc28eaa52629523ab9f882691a7bd0fa
Wilhelm Törnroos <175621+Vilkku@users.noreply.github.com>
2024-12-12 22:10:04 +0200

    Finish 6

--- 2024/6/main.go
+++ 2024/6/main.go
@@ -27,12 +27,65 @@ func main() {
 
 		guardPositionIndex := util.IndexOf(row, "^")
 
-		if (guardPositionIndex != -1) {
+		if guardPositionIndex != -1 {
 			guardPosition = [2]int{lineNumber, guardPositionIndex}
 		}
 
 		lineNumber++
 	}
 
-	fmt.Printf("Part 1: %v\n", guardPosition)
+	visited := 0
+	dirX := 0
+	dirY := -1
+
+	for {
+		fmt.Printf("Start: %v, ", guardPosition)
+
+		if rows[guardPosition[0]][guardPosition[1]] != "X" {
+			rows[guardPosition[0]][guardPosition[1]] = "X"
+			visited++
+
+			fmt.Printf("first time visiting\n")
+		} else {
+			fmt.Printf("has visited before\n")
+		}
+
+		nextPosition := [2]int{guardPosition[0] + 1*dirY, guardPosition[1] + 1*dirX}
+
+		fmt.Printf("Next position is %v", nextPosition)
+
+		if nextPosition[0] < 0 ||
+			nextPosition[0] >= len(rows) ||
+			nextPosition[1] < 0 ||
+			nextPosition[1] >= len(rows[0]) {
+			fmt.Printf(", reached the end\n\n")
+			break
+		}
+
+		if rows[nextPosition[0]][nextPosition[1]] == "#" {
+			if dirY == -1 {
+				dirX = 1
+				dirY = 0
+			} else if dirY == 1 {
+				dirX = -1
+				dirY = 0
+			} else if dirX == -1 {
+				dirX = 0
+				dirY = -1
+			} else if dirX == 1 {
+				dirX = 0
+				dirY = 1
+			}
+
+			fmt.Printf(", contains a rock\n")
+
+			nextPosition = [2]int{guardPosition[0] + 1*dirY, guardPosition[1] + 1*dirX}
+		}
+
+		fmt.Printf("Going to %v\n\n", nextPosition)
+
+		guardPosition = nextPosition
+	}
+
+	fmt.Printf("Part 1: %v\n", visited)
 }
