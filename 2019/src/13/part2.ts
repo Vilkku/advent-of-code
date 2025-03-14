e46c1847572e885f06bba5a6200c6f04a1cca142
Wilhelm Törnroos <vilkku@gmail.com>
2025-03-14 21:17:02 +0200

    Add day 13 part 2

--- 2019/src/13/part2.ts
+++ 2019/src/13/part2.ts
@@ -1,48 +1,126 @@
 import { inputToIntcodeComputerMemory } from "../util/input.js";
 import { IntcodeComputer } from "../intcode/IntcodeComputer.ts";
+import type { IntcodeComputerStatus } from "../intcode/types.ts";
 
-document.getElementById("run")?.addEventListener("click", run);
+const scoreboard = document.getElementById("score") as HTMLSpanElement;
 const canvas = document.getElementById("screen") as HTMLCanvasElement;
+const autopilotOnButton = document.getElementById(
+  "autopilot-on",
+) as HTMLButtonElement;
+const autopilotOffButton = document.getElementById(
+  "autopilot-off",
+) as HTMLButtonElement;
+const programInput = document.getElementById("program") as HTMLTextAreaElement;
+const runButton = document.getElementById("run") as HTMLButtonElement;
+const youWin = document.getElementById("you-win") as HTMLDivElement;
+
+let computer: IntcodeComputer;
+let computerStatus: IntcodeComputerStatus;
+const pixels: number[][] = [];
+
+let autopilot = false;
+let paddleX = 0;
+let ballX = 0;
+
+autopilotOnButton.disabled = true;
+autopilotOffButton.disabled = true;
+youWin.classList.add("hidden");
+
+runButton.addEventListener("click", init);
+autopilotOnButton.addEventListener("click", () => {
+  autopilot = true;
+  doAutopilot();
+  autopilotOnButton.disabled = true;
+  autopilotOffButton.disabled = false;
+});
+autopilotOffButton.addEventListener("click", () => {
+  autopilot = false;
+  autopilotOnButton.disabled = false;
+  autopilotOffButton.disabled = true;
+});
+
+function init() {
+  const initialMemory = inputToIntcodeComputerMemory(programInput.value);
+  computer = new IntcodeComputer(initialMemory);
+  run();
+
+  autopilotOnButton.disabled = false;
+  autopilotOffButton.disabled = true;
+  youWin.classList.add("hidden");
+}
 
 function run() {
-  const initialMemory = inputToIntcodeComputerMemory(
-    (document.getElementById("program") as HTMLTextAreaElement).value,
-  );
-
-  const computer = new IntcodeComputer(initialMemory);
-
-  let computerStatus = computer.run();
+  computerStatus = computer.run();
   let currentOutput = [];
-  const pixels: number[][] = [];
 
-  while (computerStatus.status !== "done") {
-    switch (computerStatus.status) {
-      case "output":
-        currentOutput.push(computerStatus.output);
+  while (computerStatus.status === "output") {
+    currentOutput.push(computerStatus.output);
 
-        if (currentOutput.length === 3) {
-          const x = currentOutput[0];
-          const y = currentOutput[1];
-          const value = currentOutput[2];
+    if (currentOutput.length === 3) {
+      const x = currentOutput[0];
+      const y = currentOutput[1];
+      const value = currentOutput[2];
 
-          if (!pixels[y]) {
-            pixels[y] = [];
-          }
-
-          pixels[y][x] = value;
-
-          currentOutput = [];
+      if (x === -1 && y === 0) {
+        scoreboard.textContent = value.toString();
+      } else {
+        if (!pixels[y]) {
+          pixels[y] = [];
         }
-        break;
-      default:
-        throw new Error(`Unexpected status ${computerStatus.status}`);
+
+        pixels[y][x] = value;
+        draw(pixels);
+
+        if (value === 3) {
+          paddleX = x;
+        } else if (value === 4) {
+          ballX = x;
+        }
+      }
+
+      currentOutput = [];
     }
 
     computerStatus = computer.run();
   }
 
-  draw(pixels);
+  if (autopilot && computerStatus.status === "input") {
+    setTimeout(doAutopilot, 5);
+  }
+
+  if (computerStatus.status === "done") {
+    draw(pixels);
+    youWin.classList.remove("hidden");
+  }
 }
+
+addEventListener("keydown", (event: KeyboardEvent) => {
+  if (
+    autopilot ||
+    !computer ||
+    !computerStatus ||
+    computerStatus.status !== "input"
+  ) {
+    return;
+  }
+
+  switch (event.key) {
+    case "ArrowRight":
+      computer.enqueueInput(1);
+      run();
+      break;
+    case "ArrowUp":
+      computer.enqueueInput(0);
+      run();
+      break;
+    case "ArrowLeft":
+      computer.enqueueInput(-1);
+      run();
+      break;
+    default:
+      break;
+  }
+});
 
 function draw(pixels: number[][]) {
   const ctx = canvas.getContext("2d") as CanvasRenderingContext2D;
@@ -85,4 +163,18 @@ function draw(pixels: number[][]) {
   });
 
   ctx.save();
+}
+
+function doAutopilot() {
+  if (computerStatus.status === "input") {
+    if (paddleX > ballX) {
+      computer.enqueueInput(-1);
+    } else if (paddleX < ballX) {
+      computer.enqueueInput(1);
+    } else {
+      computer.enqueueInput(0);
+    }
+
+    run();
+  }
 }
