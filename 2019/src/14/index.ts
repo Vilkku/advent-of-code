b15bf6c9da81e9355bd581333cec9cebb00eaab2
Wilhelm Törnroos <vilkku@gmail.com>
2025-04-06 21:13:03 +0300

    Add benchmark and asserts to day 14

--- 2019/src/14/index.ts
+++ 2019/src/14/index.ts
@@ -4,13 +4,25 @@ import {
   parseReactions,
   type Reaction,
 } from "./funcs.ts";
+import { expect } from "bun:test";
 
 import { getInput } from "../util/input.ts";
+import { useBenchmark } from "../util/benchmark.ts";
 
 const reactions: Reaction[] = parseReactions(
   await getInput(import.meta.dir, "input.txt"),
 );
 
-console.log("Part 1", getRequiredOre(reactions));
+const benchmark = useBenchmark();
 
-console.log("Part 2", maxFuelForOre(reactions));
+benchmark.reset();
+const part1Answer = getRequiredOre(reactions);
+console.log("Part 1", part1Answer);
+benchmark.get();
+expect(part1Answer).toBe(870051);
+
+benchmark.reset();
+const part2Answer = maxFuelForOre(reactions);
+console.log("Part 2", part2Answer);
+expect(part2Answer).toBe(1863741);
+benchmark.get();
