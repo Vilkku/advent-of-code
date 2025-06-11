7feced5081f37fa86e01523efe6663e2dd5cf232
Wilhelm Törnroos <175621+Vilkku@users.noreply.github.com>
2025-06-11 22:27:11 +0300

    Add day 21 part 2

--- 2019/src/21/funcs.ts
+++ 2019/src/21/funcs.ts
@@ -1,13 +1,25 @@
 import { IntcodeComputer } from "../intcode/IntcodeComputer.ts";
 
-type ReadOnlyRegister = "A" | "B" | "C" | "D";
+type ReadOnlyWalkRegister = "A" | "B" | "C" | "D";
+type ReadOnlRunRegister = "E" | "F" | "G" | "H" | "I";
 type WritableRegister = "T" | "J";
-type Register = ReadOnlyRegister | WritableRegister;
 type InstructionType = "AND" | "OR" | "NOT";
-export type Instruction = `${InstructionType} ${Register} ${WritableRegister}`;
-export type Program = Instruction[];
 
-function compileProgram(program: Program): number[] {
+type WalkRegister = ReadOnlyWalkRegister | WritableRegister;
+type RunRegister = ReadOnlyWalkRegister | ReadOnlRunRegister | WritableRegister;
+
+export type WalkInstruction =
+  `${InstructionType} ${WalkRegister} ${WritableRegister}`;
+export type RunInstruction =
+  `${InstructionType} ${RunRegister} ${WritableRegister}`;
+
+export type WalkProgram = WalkInstruction[];
+export type RunProgram = RunInstruction[];
+
+export function compileProgram(
+  program: WalkProgram | RunProgram,
+  mode: "WALK" | "RUN" = "WALK",
+): number[] {
   if (program.length > 15) {
     throw new Error(
       "Program contains more than the maximum of 15 instructions",
@@ -15,17 +27,21 @@ function compileProgram(program: Program): number[] {
   }
 
   const ASCII_NEWLINE = 10;
-  return [...program, "WALK"].flatMap((i) => [
+  return [...program, mode].flatMap((i) => [
     ...i.split("").map((c) => c.charCodeAt(0)),
     ASCII_NEWLINE,
   ]);
 }
 
-export function runProgram(initialMemory: number[], program: Program) {
+export function runProgram(
+  initialMemory: number[],
+  program: WalkProgram | RunProgram,
+  mode: "WALK" | "RUN" = "WALK",
+): number[] {
   const computer = new IntcodeComputer(initialMemory);
   let computerStatus = computer.run();
-  const asciiCodes = compileProgram(program);
-  const output = [];
+  const asciiCodes = compileProgram(program, mode);
+  const output: number[] = [];
 
   while (computerStatus.status !== "done") {
     switch (computerStatus.status) {
--- 2019/src/21/index.ts
+++ 2019/src/21/index.ts
@@ -1,6 +1,6 @@
 import { getInput, inputToIntcodeComputerMemory } from "../util/input.ts";
 import { printAnswer } from "../util/benchmark.ts";
-import { type Program, runProgram } from "./funcs.ts";
+import { type WalkProgram, runProgram, type RunProgram } from "./funcs.ts";
 
 const initialMemory = inputToIntcodeComputerMemory(
   await getInput(import.meta.dir, "input.txt"),
@@ -12,7 +12,7 @@ printAnswer(
     // Jump if one of these is true:
     // - C is hole and D is ground
     // - A is hole
-    const program: Program = [
+    const program: WalkProgram = [
       "OR D J",
       "NOT C T",
       "AND T J",
@@ -29,4 +29,33 @@ printAnswer(
     throw new Error("Did not make it across");
   },
   19359533,
+);
+
+printAnswer(
+  "Part 2",
+  () => {
+    // Jump if:
+    // - A, B or C is hole and D is ground and E or H is ground
+    const program: RunProgram = [
+      "NOT A J",
+      "NOT B T",
+      "OR T J",
+      "NOT C T",
+      "OR T J",
+      "AND D J",
+      "NOT E T",
+      "NOT T T",
+      "OR H T",
+      "AND T J",
+    ];
+    const output = runProgram(initialMemory, program, "RUN");
+
+    if (output[output.length - 1] > 65535) {
+      return output[output.length - 1];
+    }
+
+    console.log(String.fromCharCode(...output));
+    throw new Error("Did not make it across");
+  },
+  1140310551,
 );
