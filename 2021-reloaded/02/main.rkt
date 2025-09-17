5d80e4d51e2ec1202d996ecf3151d968816d0b19
Wilhelm Törnroos <175621+Vilkku@users.noreply.github.com>
2025-09-17 21:22:15 +0300

    Add day 2 part 2

--- 2021-reloaded/02/main.rkt
+++ 2021-reloaded/02/main.rkt
@@ -14,8 +14,24 @@
         ))
     (* depth horizontal)))
 
+(define (part2 input-rows)
+  (let ((depth 0) (horizontal 0) (aim 0))
+    (for ([i (in-range (length input-rows))])
+      (let ((parts (string-split (list-ref input-rows i))))
+        (match (first parts)
+          ["forward" (set! horizontal (+ horizontal (string->number (second parts)))) (set! depth (+ depth (* aim (string->number (second parts)))))]
+          ["down" (set! aim (+ aim (string->number (second parts))))]
+          ["up" (set! aim (- aim (string->number (second parts))))]
+          )
+        ))
+    (* depth horizontal)))
+
 (define input-rows (read-input "02/input.txt"))
 
 (define part1-answer (part1 input-rows))
 (printf "Part 1: ~a\n" part1-answer)
 (check-equal? part1-answer 1694130)
+
+(define part2-answer (part2 input-rows))
+(printf "Part 2: ~a\n" part2-answer)
+(check-equal? part2-answer 1698850445)
