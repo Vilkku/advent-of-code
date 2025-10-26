94de4169e68eb5652691a75269b23b37815eb61a
Wilhelm Törnroos <175621+Vilkku@users.noreply.github.com>
2025-10-26 17:42:05 +0200

    Imrpove day 6 code style

--- 2021-reloaded/06/main.exs
+++ 2021-reloaded/06/main.exs
@@ -3,6 +3,9 @@ import ExUnit.Assertions
 input = File.read!("input.txt")
 
 defmodule Day6 do
+  @days_p1 80
+  @days_p2 256
+
   defp parse_input(input) do
     input
     |> String.trim()
@@ -10,18 +13,18 @@ defmodule Day6 do
     |> Enum.map(&String.to_integer/1)
   end
 
-  defp simulate_step(initial_step) do
-    initial_step
-    |> Enum.flat_map(fn timer ->
-      if timer > 0, do: [timer - 1], else: [6, 8]
-    end)
-  end
+  defp evolve(0), do: [6, 8]
+  defp evolve(t), do: [t - 1]
+
+  defp simulate_step(timers), do: Enum.flat_map(timers, &evolve/1)
+
+  defp simulate(timers, 0), do: timers
+  defp simulate(timers, days), do: timers |> simulate_step() |> simulate(days - 1)
 
   def part1(input) do
-    values = parse_input(input)
-
-    1..80
-    |> Enum.reduce(values, fn _, acc -> simulate_step(acc) end)
+    input
+    |> parse_input()
+    |> simulate(@days_p1)
     |> length()
   end
 
@@ -30,18 +33,18 @@ defmodule Day6 do
     for i <- 0..8, do: Map.get(freq, i, 0)
   end
 
-  defp step([c0, c1, c2, c3, c4, c5, c6, c7, c8]) do
+  defp simulate_list_step([c0, c1, c2, c3, c4, c5, c6, c7, c8]) do
     [c1, c2, c3, c4, c5, c6, c7 + c0, c8, c0]
   end
 
   defp simulate_list(counts, 0), do: counts
-  defp simulate_list(counts, days), do: counts |> step() |> simulate_list(days - 1)
+  defp simulate_list(counts, days), do: counts |> simulate_list_step() |> simulate_list(days - 1)
 
   def part2(input) do
     input
     |> parse_input()
     |> get_initial_counts()
-    |> simulate_list(256)
+    |> simulate_list(@days_p2)
     |> Enum.sum()
   end
 end
